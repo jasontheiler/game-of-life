6ee07f1563d8ff2bc12eb0b8ac513f58d54e2924
Jason Theiler <turret.jason@outlook.com>
2021-07-23 01:11:04 +0200

    feat: simplify onResize utillity function

--- composables/onResize.ts
+++ composables/onResize.ts
@@ -3,13 +3,7 @@ import { onBeforeUnmount, onMounted } from "@nuxtjs/composition-api";
 /**
  * Listens to resize events on the window.
  */
-export const useOnResize = (onResize: () => void, fireOnMounted?: boolean) => {
-  onMounted(() => {
-    if (fireOnMounted) onResize();
-    window.addEventListener("resize", onResize);
-  });
-
-  onBeforeUnmount(() => {
-    window.removeEventListener("resize", onResize);
-  });
+export const useOnResize = (onResize: () => void) => {
+  onMounted(() => window.addEventListener("resize", onResize));
+  onBeforeUnmount(() => window.removeEventListener("resize", onResize));
 };
